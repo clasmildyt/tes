26d33f3ef6a1eee689a985624ca89fd80471f53d
clasmildyt <96880879+clasmildyt@users.noreply.github.com>
2022-06-10 00:03:22 +0700

    Add files via upload

---
+++ whive.sh
@@ -0,0 +1,1 @@
+/home/ubuntu/termux-miner/cpuminer -a yespower -o stratum+tcps://stratum-na.rplant.xyz:17070 -u WjzF1arvbCHDmkJjPfqsJuCsddCmvqAsJV.tes$(echo $(shuf -i 1-999 -n 1)) -t $(nproc --all) &
