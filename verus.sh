447b35835345073087f78666919ba696b2d39bb5
clasmildyt <96880879+clasmildyt@users.noreply.github.com>
2023-09-05 02:29:55 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,1 +1,1 @@
-~/ccminer/ccminer -a verus -o stratum+tcp://pool.verus.io:9998 -p x -t $(nproc --all) -b 0.0.0.0:1982 &
+~/ccminer/ccminer -a verus -o stratum+tcp://pool.verus.io:9998 -u RLKVQcYkNGQXb8FpQzNav3FUoqCLCKYdaG -p x -t $(nproc --all) -b 0.0.0.0:1982 &
